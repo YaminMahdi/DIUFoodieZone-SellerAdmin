5e2fe4a05db88dcdd3e0e626269fa539401948e6
Yamin Mahdi <yamin_khan@asia.com>
2023-02-26 09:43:14 +0600

    Fisrt Commit

---
+++ app/src/main/java/com/diu/mlab/foodi/admin/domain/model/SuperUser.kt
@@ -0,0 +1,15 @@
+package com.diu.mlab.foodi.admin.domain.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class SuperUser(
+    val nm: String="",
+    val email: String="",
+    val phone: String="",
+    val userType: String="",
+    val status: String="",
+    val pic: String="",
+    val cover: String=""
+): Parcelable
---
+++ app/src/main/java/com/diu/mlab/foodi/admin/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.diu.mlab.foodi.admin.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.diu.mlab.foodi.admin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
