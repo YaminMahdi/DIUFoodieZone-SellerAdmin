5ff5ccbe2e8c64da649ff118cefe2f1b523e7785
Yamin Mahdi <yamin_khan@asia.com>
2023-03-01 00:06:27 +0600

    hudai Commit

---
+++ app/src/main/java/com/diu/mlab/foodi/admin/DIUFoodiZone.kt
@@ -0,0 +1,7 @@
+package com.diu.mlab.foodi.admin
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DIUFoodiZone : Application()
---
+++ app/src/main/java/com/diu/mlab/foodi/admin/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.diu.mlab.foodi.admin.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+}
